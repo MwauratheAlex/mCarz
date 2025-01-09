940243b5f8f38696e403f2d4ca31ccbf1a56b40a
Mbugua <mwaurathealex@gmail.com>
2025-01-09 13:22:11 +0300

    header ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,9 @@
-import { GiCargoShip } from "react-icons/gi";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="px-4">
       <Header />
     </div>
-  );
-}
-
-function Header() {
-  return (
-    <header className="flex justify-between border border-red-500 py-4">
-      <Logo />
-      <ul className="flex gap-4">
-        <li>Vehicles</li>
-        <li>Bikes</li>
-        <li>Sell Your Car</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>FAQ</li>
-        <li><ThemeChangeBtn /></li>
-      </ul>
-    </header>
-  );
-}
-
-function Logo() {
-  return (
-    <div className="font-semibold text-xl font-mono">
-      <GiCargoShip size={70} />
-      mCarz
-    </div>
-  );
-}
-
-function ThemeChangeBtn() {
-  return (
-    <button>Themes</button>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,71 @@
+
+import Image from "next/image";
+import logoImg from "@/images/logo.svg"
+import { FaMoon } from "react-icons/fa";
+
+import { ChevronDown } from "lucide-react";
+
+export function Header() {
+    return (
+        <header className="flex justify-between items-center">
+            <Logo />
+            <ul className="flex gap-4">
+                <li><NavDropDown text="Vehicles" /></li>
+                <li><NavDropDown text="Bikes" /></li>
+                <li><NavLink text="Sell Your Car" /></li>
+                <li><NavLink text="About" /></li>
+                <li><NavLink text="Contact" /></li>
+                <li><NavLink text="FAQ" /></li>
+                <li><ThemeChangeBtn /></li>
+            </ul>
+        </header>
+    );
+}
+
+function Logo() {
+    return (
+        <div className="text-center">
+            <Image src={logoImg} alt="logo image" className="w-20" />
+            <div className="text-sm font-mono tracking-wider">
+                mCarz
+            </div>
+        </div>
+    );
+}
+
+function NavDropDown(props: { text: string }) {
+    return (
+        <div className="daisy-dropdown">
+            <div tabIndex={0} role="button" className="daisy-btn daisy-btn-ghost daisy-btn-sm">
+                <div className="flex gap-1 items-center font-normal">
+                    <span>
+                        {props.text}
+                    </span>
+                    <ChevronDown className="font-normal w-4" />
+                </div>
+            </div>
+            <ul tabIndex={0} className="daisy-dropdown-content daisy-menu bg-base-100 rounded-box z-[1] p-2 w-max shadow">
+                <li><a>All {props.text}</a></li>
+                <li><a>Available in Kenya</a></li>
+                <li><a>Direct Import/International</a></li>
+            </ul>
+        </div>
+
+    );
+}
+
+
+function NavLink(props: { text: string }) {
+    return (
+        <a className="daisy-btn daisy-btn-ghost daisy-btn-sm font-normal">{props.text}</a>
+    )
+}
+
+
+function ThemeChangeBtn() {
+    return (
+        <button className="daisy-btn daisy-btn-sm daisy-btn-square daisy-btn-ghost">
+            <FaMoon />
+        </button>
+    );
+}
