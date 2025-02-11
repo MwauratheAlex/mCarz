f7d83812f6c1a3ce100f1bdc25dd5b4efd926115
Mbugua <mwaurathealex@gmail.com>
2025-02-11 15:10:05 +0300

    force static again

--- src/app/vehicles/[id]/page.tsx
+++ src/app/vehicles/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { PaddingWrapper } from "@/components/ui/PaddingWrapper";
 import { Vehicle } from "@/components/Vehicle";
 
+export const dynamic = "force-static"
+
 export default function VehiclePage() {
   return (
     <PaddingWrapper>
