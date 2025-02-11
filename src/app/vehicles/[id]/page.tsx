f68ed741c3a1fb90b8e30816de659d3504f3a302
Mbugua <mwaurathealex@gmail.com>
2025-02-11 12:29:55 +0300

    remove force static

--- src/app/vehicles/[id]/page.tsx
+++ src/app/vehicles/[id]/page.tsx
@@ -1,8 +1,6 @@
 import { PaddingWrapper } from "@/components/ui/PaddingWrapper";
 import { Vehicle } from "@/components/Vehicle";
 
-export const dynamic = "force-static"
-
 export default async function VehiclePage({ params }: {
   params: Promise<{ id: string }>
 }) {
